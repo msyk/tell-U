48f3a1b421169ad3f1cdecb8ac215fe4ff527dea
Masayuki Nii <msyk@msyk.net>
2015-10-20 01:00:49 +0900

    Initial file arrangements

---
+++ contents.js
@@ -0,0 +1,143 @@
+/**
+ * Created by msyk on 15/10/12.
+ */
+
+var config = {
+    theme: "msyk1",
+    transition: "fade"
+};
+
+var contents = [
+    {
+        presubtitle: "The subtitle for you",
+        maintitle: "Your Best Presentation Title",
+        subtitle: "The another subtitle for you",
+        coveritems: [
+            "The date of presentation so on.",
+            "Your name, who are you?",
+            "Your company, your title of position",
+            "Your email, Twitter account."
+        ]
+    },
+    {
+        title: "A Good Title for You!",
+        items: [
+            "First gread item",
+            "-Happy sub items you knows who knows",
+            "--OK! sub-sub items you knows who knows",
+            "--NO! sub-sub items you knows who knows",
+            "Second stunning item",
+            "-Happy sub items you knows who knows",
+            "-Happy sub items you knows who knows",
+            "Third best item",
+            "-Happy sub items you knows who knows",
+            "-Happy sub items you knows who knows",
+            "Fource forced item",
+            "-Happy sub items you knows who knows",
+            "-Happy sub items you knows who knows"
+        ]
+    },
+    {
+        title: "A Long Long Long Long Long Long Long Long Long Long Long Long Long Long Title for You!",
+        items: [
+            "First sock item. First sock item. First sock item. First sock item. First sock item. First sock item. First sock item. ",
+            "-Sad sub items.Sad sub items.Sad sub items.Sad sub items.Sad sub items.Sad sub items.Sad sub items.Sad sub items.",
+            "-Sad sub items.Sad sub items.Sad sub items.Sad sub items.Sad sub items.Sad sub items.Sad sub items.Sad sub items.",
+            "First sock item. First sock item. First sock item. First sock item. First sock item. First sock item. First sock item. ",
+            "-Sad sub items.Sad sub items.Sad sub items.Sad sub items.Sad sub items.Sad sub items.Sad sub items.Sad sub items.",
+            "-Sad sub items.Sad sub items.Sad sub items.Sad sub items.Sad sub items.Sad sub items.Sad sub items.Sad sub items.",
+            "First sock item. First sock item. First sock item. First sock item. First sock item. First sock item. First sock item. ",
+            "-Sad sub items.Sad sub items.Sad sub items.Sad sub items.Sad sub items.Sad sub items.Sad sub items.Sad sub items.",
+            "-Sad sub items.Sad sub items.Sad sub items.Sad sub items.Sad sub items.Sad sub items.Sad sub items.Sad sub items.",
+            "First sock item. First sock item. First sock item. First sock item. First sock item. First sock item. First sock item. ",
+            "-Sad sub items.Sad sub items.Sad sub items.Sad sub items.Sad sub items.Sad sub items.Sad sub items.Sad sub items.",
+            "-Sad sub items.Sad sub items.Sad sub items.Sad sub items.Sad sub items.Sad sub items.Sad sub items.Sad sub items.",
+            "First sock item. First sock item. First sock item. First sock item. First sock item. First sock item. First sock item. ",
+            "-Sad sub items.Sad sub items.Sad sub items.Sad sub items.Sad sub items.Sad sub items.Sad sub items.Sad sub items.",
+            "-Sad sub items.Sad sub items.Sad sub items.Sad sub items.Sad sub items.Sad sub items.Sad sub items.Sad sub items.",
+        ]
+    },
+    {
+        title: "A Good Title for You!",
+        items: [
+            "First gread item",
+            "-Happy sub items you knows who knows",
+            "-Happy sub items you knows who knows",
+            "Second stunning item",
+            "-Happy sub items you knows who knows",
+            "-Happy sub items you knows who knows",
+            "Third best item",
+            "-Happy sub items you knows who knows",
+            "-Happy sub items you knows who knows",
+            "Fource forced item",
+            "-Happy sub items you knows who knows",
+            "-Happy sub items you knows who knows"
+        ],
+        fig1: "images/photo1.jpg"
+    },
+    {
+        title: "A Good Title for You!",
+        items: [
+            "First gread item",
+            "-Happy sub items you knows who knows",
+            "-Happy sub items you knows who knows",
+            "Second stunning item",
+            "-Happy sub items you knows who knows",
+            "-Happy sub items you knows who knows",
+            "Third best item",
+            "-Happy sub items you knows who knows",
+            "-Happy sub items you knows who knows",
+            "Fource forced item",
+            "-Happy sub items you knows who knows",
+            "-Happy sub items you knows who knows"
+        ],
+        fig1: "images/photo1.jpg",
+        maximagewidth: 20
+    },
+    {
+        title: "A Good Title for You!",
+        items: [
+            "First gread item",
+            "-Happy sub items you knows who knows",
+            "-Happy sub items you knows who knows",
+            "Second stunning item",
+            "-Happy sub items you knows who knows",
+            "-Happy sub items you knows who knows",
+            "Third best item",
+            "-Happy sub items you knows who knows",
+            "-Happy sub items you knows who knows",
+            "Fource forced item",
+            "-Happy sub items you knows who knows",
+            "-Happy sub items you knows who knows"
+        ],
+        fig1: "images/photo1.jpg",
+        fig3: "images/photo3.jpg"
+    },
+    {
+        centertitle: "A Bad Title for You!",
+        centeritems: [
+            "First sock item",
+            "-Sad sub items"
+        ]
+    },
+    {
+        title: "A Bad Title for You!",
+        fig2: "images/photo2.jpg"
+    },
+    {
+        title: "A Bad Title for You!",
+        fig1: "images/photo1.jpg",
+        fig3: "images/photo3.jpg"
+    },
+    {
+        title: "A Bad Title for You!",
+        fig1: "images/photo1.jpg",
+        fig2: "images/photo2.jpg",
+        fig3: "images/photo3.jpg"
+    },
+    {
+        title: "A Bad Title for You!",
+        fig1: "images/photo1.jpg"
+    }
+];
+
