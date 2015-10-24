b0147d75e1e4082093c2ecf612e4ad31c4a88ef8
Masayuki Nii <msyk@msyk.net>
2015-10-25 00:51:16 +0900

    Refactoring

--- projector/slidecontrol.js
+++ projector/slidecontrol.js
@@ -7,7 +7,9 @@ var currentId = 0;
 var slideNodes = null;
 
 window.onload = function () {
-    var nodeSlide = document.getElementsByClassName("slide")[0];
+    var nodeSlide, headNode, linkNode;
+
+    nodeSlide = document.getElementsByClassName("slide")[0];
     slideNodes = nodeSlide.cloneNode(true);
     nodeSlide.parentNode.removeChild(nodeSlide);
 
@@ -15,85 +17,74 @@ window.onload = function () {
         slideNumber = parseInt(document.location.hash.substr(1)) - 1;
     }
 
-    var headNode = document.getElementsByTagName("HEAD")[0];
-    var linkNode = document.createElement("LINK");
+    headNode = document.getElementsByTagName("HEAD")[0];
+    linkNode = document.createElement("LINK");
     linkNode.setAttribute("rel", "stylesheet");
     linkNode.setAttribute("href", "projector/themes/" + config.theme + "/slidestyle.css");
     headNode.appendChild(linkNode);
+
     setTimeout(function () {
         adjust(loadSlide(true, document.body));
     }, 100);
+};
 
-    window.onclick = function (e) {
-        if (e.clientX > window.innerWidth / 2) {
-            moveSlide(+1);
-        } else {
-            moveSlide(-1);
-        }
-    };
+window.onclick = function (e) {
+    if (e.clientX > window.innerWidth / 2) {
+        moveSlide(+1);
+    } else {
+        moveSlide(-1);
+    }
+};
 
-    document.body.onkeydown = function (e) {
-        var url;
-        switch (e.keyCode) {
-            case 37:
-            case 38:
-                if (!e.shiftKey) {
-                    moveSlide(-1);
-                } else {
-                    moveSlide(0, true);
-                }                break;
-            case 39:
-            case 40:
-                if (!e.shiftKey) {
-                    moveSlide(+1);
-                } else {
-                    moveSlide(-1, true);
-                }                break;
-            case 49:    // keyCode = 49 for "1"
+window.onkeydown = function (e) {
+    switch (e.keyCode) {
+        case 37:
+        case 38:
+            if (!e.shiftKey) {
+                moveSlide(-1);
+            } else {
                 moveSlide(0, true);
-                break;
-            case 69:    // keyCode = 69 for "E"
+            }
+            break;
+        case 39:
+        case 40:
+            if (!e.shiftKey) {
+                moveSlide(+1);
+            } else {
                 moveSlide(-1, true);
-                break;
-            case 70:    // keyCode = 70 for "F"
-                var element = document.documentElement;
-                if (element.requestFullscreen) {
-                    element.requestFullscreen();
-                } else if (element.msRequestFullscreen) {
-                    element.msRequestFullscreen();
-                } else if (element.mozRequestFullScreen) {
-                    element.mozRequestFullScreen();
-                } else if (element.webkitRequestFullscreen) {
-                    element.webkitRequestFullscreen(element.ALLOW_KEYBOARD_INPUT);
-                }
-                break;
-            case 82:    // keyCode = 82 for "R"
-            case 13:    // keyCode = 13 for Return
-                url = document.URL;
-                if (url.match(/#/)) {
-                    url = url.split(/#/)[0];
-                }
-                document.location.href = url + "#" + (slideNumber + 1);
-                location.reload();
-                break;
-        }
-        e.preventDefault();
-    };
+            }
+            break;
+        case 49:    // keyCode = 49 for "1"
+            moveSlide(0, true);
+            break;
+        case 69:    // keyCode = 69 for "E"
+            moveSlide(-1, true);
+            break;
+        case 70:    // keyCode = 70 for "F"
+            fullScreen();
+            break;
+        case 82:    // keyCode = 82 for "R"
+        case 13:    // keyCode = 13 for Return
+            setupURL();
+            location.reload();
+            break;
+    }
+    e.preventDefault();
+};
 
-    window.onresize = function () {
-        adjust();
-    };
-    
-    window.onhashchange = function () {
-		location.reload();
-    };
+window.onresize = function () {
+    adjust();
+};
 
-}
+window.onhashchange = function () {
+    location.reload();
+};
+
 function moveSlide(bias, absolute) {
     var slide;
-    if (absolute)   {
+    if (absolute) {
         slideNumber = bias;
-        if (bias < 0)   {
+        if (bias < 0) {
             slideNumber = contents.length + bias;
         }
     }
@@ -106,15 +97,34 @@ function moveSlide(bias, absolute) {
         slideNumber = 0;
     } else {
         slide = loadSlide(false, document.body);
-        
-        url = document.URL;
-        if (url.match(/#/)) {
-            url = url.split(/#/)[0];
-        }
-        document.location.href = url + "#" + (slideNumber + 1);
+        setupURL();
 
         setTimeout(function () {
             adjust(slide);
         }, 100);
+    }
+}
+
+function setupURL() {
+    var url;
+
+    url = document.URL;
+    if (url.match(/#/)) {
+        url = url.split(/#/)[0];
+    }
+    document.location.href = url + "#" + (slideNumber + 1);
+}
+
+function fullScreen() {
+    console.log("#");
+    var element = document.documentElement;
+    if (element.requestFullscreen) {
+        element.requestFullscreen();
+    } else if (element.msRequestFullscreen) {
+        element.msRequestFullscreen();
+    } else if (element.mozRequestFullScreen) {
+        element.mozRequestFullScreen();
+    } else if (element.webkitRequestFullscreen) {
+        element.webkitRequestFullscreen(element.ALLOW_KEYBOARD_INPUT);
     }
 }
